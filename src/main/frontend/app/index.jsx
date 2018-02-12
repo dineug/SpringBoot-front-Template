26214708928c0348fb9e984c32822eaf7f95c19d
LSH\lsh <dlrnld@jongdali.com>
2018-02-12 10:12:20 +0900

    react 랜더링 최신버전으로 변경

--- src/main/frontend/app/index.jsx
+++ src/main/frontend/app/index.jsx
@@ -2,6 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../style/style.css'
 
-ReactDOM.render(
-	<h1 className="testblue">App working</h1>,
-	document.getElementById('root'));
+class Tick extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+      }
+    
+    componentDidMount() {
+        this.timerID = setInterval(
+                () => this.tick(),
+                1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+    
+    tick() {
+        this.setState({
+          date: new Date()
+        });
+      }
+    
+    render(){
+        return (
+          <div>
+                <h1>tiem : {this.state.date.toLocaleTimeString()}</h1>
+          </div>      
+        );
+    }
+}
+
+
+ReactDOM.render(<Tick />, document.getElementById('root'));
